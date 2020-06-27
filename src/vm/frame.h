3da69fd2551c0da8b575997b1cf78c00d770e605
XunZhiyang <xunzhiyang@sjtu.edu.cn>
2020-06-27 16:37:40 +0800

    Add "frame.h"

---
+++ src/vm/frame.h
@@ -0,0 +1,29 @@
+//
+// Created by sheepp on 2020/6/26.
+//
+
+#ifndef PINTOS_FRAME_H
+#define PINTOS_FRAME_H
+
+struct frame_entry {
+    void *frame;
+    void *upage;
+    struct thread *thread;
+    struct hash_elem hash_elem;
+    struct list_elem list_elem;
+    bool pinned;
+};
+
+void *frame_init();
+
+void *frame_query(void *frame);
+
+void *frame_get(enum palloc_flags, void *upage);
+
+void *frame_free(void *frame);
+
+bool frame_if_pinned(void *frame);
+
+bool frame_release_pinned(void *frame);
+
+#endif //PINTOS_FRAME_H
